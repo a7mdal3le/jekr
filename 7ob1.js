bf8c7c6e602595fffbfcbe0870e36d84fcb38a0b
jekr <48930765+a7mdal3le@users.noreply.github.com>
2020-05-24 01:02:53 +0300

    Create 7ob1.js

---
+++ 7ob1.js
@@ -0,0 +1,132 @@
+$( "a.label.label-primary.fl > img.fl" ).replaceWith('<img src="" class="fl" style="margin-right: 2px; width: 25px;">');$('a[href="https://jawalhost.com/"]').removeAttr('href');
+$(".checkbox label").html(`<input id="stealth" type="checkbox" value="">`);
+eval((close+'').replace('location.reload()','location.href="/"'));
+$('.nav-tabs').addClass('fl').css('width','100%');
+$("[data-target=#wall]").attr('title','♥ مسـاحه حـرهـ لإبداعـاتكم ♥')
+$('.stopic').attr('readonly',true).css('background-color','#eeeeee')
+$("div#tlogins button.btn.btn-primary").click(function(){setTimeout(function(){refreshonline()},0)}),setInterval(function(){var b=$("#dpnl");0<b.length&&b.remove()},0),$(".dpnl").append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><center></div><center></div>`);
+_setprofile=setprofile;
+setprofile=function (){
+    if(getuser(myid).rep<5){return;}
+    return _setprofile();
+}
+
+setprofile_=setprofile;
+reqlikes=10;
+setprofile=function(){if(getuser(myid).rep<reqlikes){alert(' يتطلب منك عليك  الحصول من الادارة لايك '+reqlikes+''); return;}else{setprofile_();}}
+$(` <center><div style=" width: 99%;height: auto!important; "  id="sa" margin:1px;" class="fa fa-heartbeat btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+	<font color="#ff3300">&nbsp;  ·( اذاعة شات الحب)· &#9679; &nbsp;</font></div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>
+	<b><font color="#000000" size="5">اريحوا مسامعكم </font></b></p><audio controls loop="" play=""><source src="https://h.top4top.io/m_1595scph70.mp3"></audio></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary')
+$(`<style>
+.rpwd {width: 60%!important;border-radius: 50px!important;border:2px solid;}
+.uroomz.border {width: fit-content!important;border-radius: 50px!important;margin-top: -46px!important;margin-left: 200px!important;}
+.roomz.selbox.form-control {width: 100%!important;border:2px solid;border-radius: 50px!important;}
+.upower.border {width: fit-content!important;border-radius: 50px!important;margin-top: -47px!important;margin-left: 190px!important;}
+.userpower.selbox.form-control {width: 100%!important;border:2px solid;border-radius: 50px!important;}
+.userdays {width: 56%!important;border-radius: 50px!important;border:2px solid;}
+.ulikec.border {width: fit-content!important;border-radius: 50px!important;border:2px solid;}
+.likec {width: 56%!important;border-radius: 50px!important;border:2px solid;}
+.ae.fa.chats.label.fa-comment.label-primary {border-radius: 0px 0px 0px 0px!important}
+.ae.fa.label.label-primary.fa-user {border-radius: 30px 0px 0px 30px!important;}
+.ae.fa.label.label-primary.fa-users {border-radius: 0px 0px 0px 0px!important;margin-left: 0px!important;}
+.ae.fa.label.label-primary.fa-commenting-o {border-radius: 0px 0px 0px 0px!important;margin-left: 0px!important;}
+.ae.label.label-primary.fa.fa-gear {border-radius: 0px 30px 30px 0px!important;margin-left: 0px!important;}
+
+</style>`).insertBefore('body');
+
+
+$('.uzr.fl.corner.borderg').css({'margin': '0px 0',borderRight: '0px solid #ffffff',borderLeft: '0px solid #ffffff',borderTop: '.0px solid #ffffff',borderBottom: '.0px solid #ffffff',paddingTop: '3px',borderRadius: '0px 0px 0px 0px'});
+var Wally = function(){
+  $(".btn.minix.fa.fa-thumbs-up.fr").css("margin-right",174);
+$(".btn.minix.fa.fa-comment.fr").css("margin-Right",-22);
+$(".btn.minix.fa.fa-times.fr").css("margin-right",-10);
+};
+setInterval(Wally, 8000);
+$('.nav-tabs').addClass('fl').css('width','100%');
+$(`<style>
+}
+span.s1.fa.fa-user.label.badgex.label-as-badge.label-success
+	{
+	padding:0px 3px 2px;
+	margin-right:5px
+}
+.fr.borderg,.fr.borderg a
+	{
+	border-radius:3px!important;
+	margin-right:0px!important;
+	border:black solid 0px;
+	color:#7D7D7D;
+        
+}
+ul.nav.nav-tabs>li>a
+	{
+	color:#ffffff;
+	border:none;
+	text-align:center;
+	border-radius:3px;
+	display:initial;
+	width:80%;
+	float:left;
+	padding:0!important;
+        Background-color:#47443F;
+}
+ul.nav.nav-tabs>li.active>a
+	{
+	border:none;
+	border-radius:3px
+}
+ul.nav.nav-tabs>li
+	{
+	width:32%;
+	margin:0 5px 0 0
+}
+ul.nav.nav-tabs>li>.fa-user-plus:before,ul.nav.nav-tabs>li>.fa-user:before,ul.nav.nav-tabs>li>.fa-user:before
+	{
+	float:right;
+	margin-right:-26%;
+	border:1px solid;
+	padding:0 2px 1px;
+	border-radius:3px;
+	width:23px;
+	text-align:center;
+	color:#8E0908
+}
+ul.nav.nav-tabs
+	{
+	border-top:1px solid#190604;
+	margin-top:1px;
+	float:right;
+	width:100%;
+	padding:3px 1px
+}
+input#u1,input#u2,input#pass1,input#u3,input#pass2
+	{
+	width:50%;
+	height:31px;
+	margin-left:40.5px;
+	margin-bottom:5.5px;
+	border:1px solid#D1351D;
+	border-radius:3px;
+	text-align:center
+}
+div#asim13
+	{
+	width:49%;
+	top:122px;
+	position:absolute;
+	left:3px
+}
+div#l1
+	{
+	padding:16.8px!important
+}
+
+</style>`).insertBefore("body");
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value="" maxlength="10"><i class="fa fa-eye-slash" aria-hidden="true">`)
+$(`<font size="4" color="#ffffff"><marquee direction="right" width="100%" height="20" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5" style="height: 20px; width: 100%;background-color:#47443F;"> 
+تقبل الله طاعتكم ، وكل عام وانتم بخير ، وعيدكم مبارك .. 
+</marquee></font>`).insertBefore('.nav-tabs'); 
+
+
+
+var _0x3d29=["\x77\x69\x64\x74\x68","\x30\x70\x78","\x63\x73\x73","\x2E\x66\x6C\x2E\x75\x73\x74\x61\x74"];$(_0x3d29[3])[_0x3d29[2]](_0x3d29[0],_0x3d29[1])
