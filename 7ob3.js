d8c91a11f0f2de6da731d03cdcefe1acc5eb3202
jekr <48930765+a7mdal3le@users.noreply.github.com>
2020-05-25 12:51:18 +0300

    Create 7ob3.js

---
+++ 7ob3.js
@@ -0,0 +1,24 @@
+$( "a.label.label-primary.fl > img.fl" ).replaceWith('<img src="" class="fl" style="margin-right: 2px; width: 25px;">');$('a[href="https://jawalhost.com/"]').removeAttr('href');
+$(".checkbox label").html(`<input id="stealth" type="checkbox" value="">`);
+eval((close+'').replace('location.reload()','location.href="/"'));
+$('.nav-tabs').addClass('fl').css('width','100%');
+$("[data-target=#wall]").attr('title','♥ مسـاحه حـرهـ لإبداعـاتكم ♥')
+$('.stopic').attr('readonly',true).css('background-color','#eeeeee')
+$("div#tlogins button.btn.btn-primary").click(function(){setTimeout(function(){refreshonline()},0)}),setInterval(function(){var b=$("#dpnl");0<b.length&&b.remove()},0),$(".dpnl").append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><center></div><center></div>`);
+_setprofile=setprofile;
+setprofile=function (){
+    if(getuser(myid).rep<5){return;}
+    return _setprofile();
+}
+
+setprofile_=setprofile;
+reqlikes=10;
+setprofile=function(){if(getuser(myid).rep<reqlikes){alert(' يتطلب منك عليك  الحصول من الادارة لايك '+reqlikes+''); return;}else{setprofile_();}}
+$(` <center><div style=" width: 99%;height: auto!important; "  id="sa" margin:1px;" class="fa fa-heartbeat btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+	<font color="#ff3300">&nbsp;  ·( اذاعة شات الحب)· &#9679; &nbsp;</font></div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>
+	<b><font color="#000000" size="5">اريحوا مسامعكم </font></b></p><audio controls loop="" play=""><source src="https://h.top4top.io/m_1595scph70.mp3"></audio></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary')
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value="" maxlength="10"><i class="fa fa-eye-slash" aria-hidden="true">`)
+$(`<font size="4" color="#ffffff"><marquee direction="right" width="100%" height="20" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5" style="height: 20px; width: 100%;background-color:#47443F;"> 
+تقبل الله طاعتكم ، وكل عام وانتم بخير ، وعيدكم مبارك .. 
+</marquee></font>`).insertBefore('.nav-tabs'); 
+var _0x3d29=["\x77\x69\x64\x74\x68","\x30\x70\x78","\x63\x73\x73","\x2E\x66\x6C\x2E\x75\x73\x74\x61\x74"];$(_0x3d29[3])[_0x3d29[2]](_0x3d29[0],_0x3d29[1])
