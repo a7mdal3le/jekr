7f35db0c22cd91b851573f39289af544d76e1882
jekr <48930765+a7mdal3le@users.noreply.github.com>
2024-10-14 20:28:14 +0300

    Update mskchat.js
    تعديل

--- mskchat.js
+++ mskchat.js
@@ -1,1 +1,34 @@
-$("<div class='fr borderg' style='padding:2px;background-color:white;float:left;margin-left:4px;'>  تصميم :<a href='https://mskchat.com/'>شات الحب</a></div>").insertAfter($(".loginstat"));$(`<link rel="stylesheet" type="text/css" href=""><style>button.btn.fr.btn-success.fa.fa-refresh{display:none}a.label.label-primary.fl img.fl{display:none}a.label.label-primary.fl{border-radius:3px!important;padding:5px!important;margin-bottom:2px;text-align:Center!important}#des22{text-align:center!important;font-size:100%!important}label.fl.label.loginstat{background-color:transparent!important;position:absolute;top:0;margin-top:4px;left:0}.fr.borderg,.fr.borderg a{font-size:10px!important;padding:0 5px!important;position:relative;color:#ffffff}.fr.borderg{border:0;margin-top:0;height:0}span.s1.fa.fa-user.label.badgex.label-as-badge.label-success{padding:0 3px 2px;margin-right:5px}label.label.label-primary.mini.fl{padding:1px;border-radius:3px!important}div#l1 input#u1,div#l3 button{width:99%;margin-left:1px}div#l2 button,div#l3 button{border:1px solid #ccc!important;border-radius:24px!important;height:27px;padding:0 5px}div#l1 input#u1{border-radius:15px;border:1px solid #e1e0e0;margin-top:3px;text-align:center}div#l2 input#pass1,div#l2 input#u2,div#l3 input#pass2,div#l3 input#u3{width:49%;padding:0!important;float:right;margin:2px 5px 2px 0}div#l2 input#u2,div#l3 input#u3{margin-left:-6px;border-radius:15px;border:1px solid #e1e0e0;text-align:center}div#l2 input#pass1,div#l3 input#pass2{border-radius:15px;border:1px solid #e1e0e0;text-align:center}div#l2,div#l3{padding:2px!important}div#l1{margin:0 1px -18px;padding:0!important}div#l1,div#l2,div#l3{border-bottom:2px solid;margin-bottom:2px;border-top:2px solid;margin-top:0;border-color:#bababa;border-radius:15px;height:69px}div#l1 button{border:1px solid #ccc!important;border-radius:24px!important;width:99%;height:27px;padding:0 5px}div#l2 button{width:70%}ul.nav.nav-tabs>li>a{border:none;text-align:center;border-radius:3px;display:initial;width:80%;float:left;padding:0!important}ul.nav.nav-tabs>li.active>a{border:none;border-radius:3px}ul.nav.nav-tabs>li{width:32%;margin:0 5px 0 0}ul.nav.nav-tabs>li>.fa-user-plus:before,ul.nav.nav-tabs>li>.fa-user:before,ul.nav.nav-tabs>li>.fa-user:before{float:right;margin-right:-26%;padding:0 2px 1px;border-radius:3px;width:23px;text-align:center}.nav>li>a:focus,.nav>li>a:hover{color:#9abff9}ul.nav.nav-tabs{margin-top:2px;float:right;width:100%;padding:3px 1px}div#tlogins::-webkit-scrollbar{width:5px;height:10px}div#tlogins::-webkit-scrollbar-button{width:5px;height:5px}div#tlogins::-webkit-scrollbar-thumb{background:0 0;border:71px #fff;border-radius:31px}div#tlogins::-webkit-scrollbar-thumb:active{background:0 0}div#tlogins::-webkit-scrollbar-track{background:#fff;border:65px #fff;border-radius:34px}div#tlogins::-webkit-scrollbar-track:hover{background:0 0}div#tlogins::-webkit-scrollbar-track:active{background:0 0}div#tlogins::-webkit-scrollbar-corner{background:0 0}div#tlogins .fl.mini.u-msg{margin-top:3px!important;border-radius:3px!important}a.label.label-primary.fl img.fl,button.btn.fr.btn-success.fa.fa-refresh,div#l2 br,div#l3 br{display:none}</style>`).insertBefore('body');
+if($("#cp").length){itvx=setInterval(function(){$("#powers input[name='rjoin'").hide();},1000);} 
+$("#design-a").click(function(){$("#spanan").slideToggle(function(){});});
+$(`<link rel="stylesheet" type="text/css" href="">
+<link rel="stylesheet" href="">
+<style>
+#mic .mic {
+text-align: center;
+width:56px;
+background-image:none;
+border:2px solid #afbfc2!important;
+background-color:#ffffff;
+    border-radius: 30% 1% !important;
+    margin: 1px !important;
+    height: 47px !important;
+    background-position: center !important;
+    background-repeat: no-repeat !important;
+    background-size: cover;
+    border-radius: 30% 1% !important;
+
+}
+</style>`).insertBefore('body');
+
+if(location.protocol!='https:'){location.protocol='https:';};
+  $("#bnr").append($(`<img src="https://mskchat.com/sico/zxbde59058f56c94.png"style="width:100%;margin-top: 0px;margin-bottom: 00px;" ><center><div id="ahmed13">
+<a id="Rafat Al Assaf" href="https://al3nood.net/"style="width: 90px;padding: 1px;height: 22px;" class="btn minix btn-primary">شات قلوب</a>
+<a id="Rafat Al Assaf" href="https://mskchat.com/"style="width: 90px;padding: 1px;height: 22px;" class="btn minix btn-primary">شات فله</a>
+<a id="Rafat Al Assaf" href="https://mskchat.com/"style="width: 90px;padding: 1px;height: 22px;" class="btn minix btn-primary">شات تعب قلبي</a>
+<a id="Rafat Al Assaf" href="https://l7p.net/"style="width: 80px;padding: 1px;height: 22px;" class="btn minix btn-primary">شات الحب</a>`));
+msgt=50;bct=50; 
+showpics=-1;
+$(".umod").remove(); 
+$(".ubnr").remove();
+
+ 
